2a04d08573c0eb6d93c9dc818e344a76f562aaf8
marvin evins <maevins@cps.edu>
2017-06-26 20:46:51 -0500

    added validation
    added validation to form

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-
+var mongojs = require('mongojs');
+var db = mongojs('passportapp', ['users']);
+var bcrypt = require('bcryptjs');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 
 //login page - GET
@@ -15,5 +19,40 @@ router.get('/register', function(req, res){
   res.render('register');
 });
 
+//register post
+router.post('/register', function(req, res){
+  //get form values
+  var name                  =req.body.name;
+  var email                 =req.body.email;
+  var username              =req.body.username;
+  var password              =req.body.password;
+  var password2             =req.body.password2;
+
+//validation
+  req.checkbody('name', 'Name field is required').notEmpty();
+  req.checkbody('email', 'Email field is required').notEmpty();
+  req.checkbody('email', 'Please use a valid email address').notEmpty();
+  req.checkbody('username', 'User Name field is required').notEmpty();
+  req.checkbody('password', 'Password field is required').notEmpty();
+  req.checkbody('password2', 'Passwords do not match').equals(req.body.password);
+
+//check for errors
+  var errors = req.ValidationErrors();
+
+  if(errors){
+    console.log('form has errors...');
+    res.render('register',{
+      errors: errors,
+      name: name,
+      email: email,
+      username: username,
+      password: password,
+      password2: password2
+    });
+  }else{
+    console.log('success');
+  }
+
+});
 
 module.exports = router;
